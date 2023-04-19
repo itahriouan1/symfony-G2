b12ee9e3c059b17805e011e01f0082586adf3974
itahriouan zakaria <zakariaitahriouan@gmail.com>
2023-04-19 01:14:05 +0000

    seance 12/04

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,16 +9,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController {
 
-    /**
-     * @Route("/", name="index")
-     * @return Response
-     */
-    public function indexAction() {
+    
+    #[Route("/", name : "index")]
+    public function indexAction(): Response
+     {
         $t = [[1,2,3],[4,5,6],[7,8,9]];
         $personne = new stdClass();
         $personne->Nom = 'itahriouan';
         $personne->Prenom = 'zakaria';
         return $this->render('base.html.twig', ['p'=>$personne, 't'=>$t]);
+    }
+    #[Route("/home{id}", name : "home")]
+    public function homeAction($id): Response
+     {
+        $id = $id *2;
+        return $this->render('home.html.twig', ['id' => $id]);
     }
 
 }
---
+++ src/Entity/Stage.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StageRepository::class)]
+class Stage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $Title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $startDate = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $endDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+}
